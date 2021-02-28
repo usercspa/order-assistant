5766eb57e90b7540f8fe79a2e907cca7de79589b
usercspa <spacchbk@gmail.com>
2021-02-28 17:55:11 +0000

    flask

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 @app.route("/", methods=[]"GET")
 
 def index():
-  retuen "hi"
+  retuen render_template('index.html')
 
   if __name__ == "__main__" :
-    app.run(debug=TRUE, threaded=TRUE)  
+    app.run(debug=True, threaded=True)  
